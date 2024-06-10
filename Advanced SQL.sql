e7c18d44cc0b47d86fbec03e92d4dac126ee9e3b
VincentOracle <104081669+VincentOracle@users.noreply.github.com>
2024-06-10 15:44:03 +0300

    Update Advanced SQL.sql

--- Advanced SQL.sql
+++ Advanced SQL.sql
@@ -1,16 +1,10 @@
 /*
 *********************************************************************
-http://www.mysqltutorial.org
+
 *********************************************************************
-Name: MySQL Sample Database classicmodels
-Link: http://www.mysqltutorial.org/mysql-sample-database.aspx
-Version 3.1
-+ changed data type from DOUBLE to DECIMAL for amount columns
-Version 3.0
-+ changed DATETIME to DATE for some colunmns
-Version 2.0
-+ changed table type from MyISAM to InnoDB
-+ added foreign keys for all tables 
+NAME: WERE VINCENT
+DATE:03/JUNE/2022
+MODULE: SQL FOR DATA ANALYSIS
 *********************************************************************
 */
 
